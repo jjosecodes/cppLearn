fe28d3ed91bd4e157105b3e7930a15c1ddaea251
jjosecodes <josejob123121@gmail.com>
2024-09-10 15:03:26 -0500

    initial commit

---
+++ const.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+int main(){
+
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    const int LIGHTSPEED = 299792458;
+
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+    
+
+    std::cout << circumference << " cm";
+
+
+    return 0;
+}
+
+
+
