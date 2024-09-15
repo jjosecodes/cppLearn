587882c7d8e44d229b981852165a770957d041ee
jjosecodes <josejob123121@gmail.com>
2024-09-14 23:43:05 -0500

    atithmatic operator

---
+++ c++_brocode/arithmetic.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+/*
+arithmetic operators
+* follows PEMDAS *
+- perenthesis
+- multiplication and division
+- addition and subtraction
+
+
+*/
+
+
+int main (){
+
+    int students = 20; 
+    // // addition operator
+    // students = students +1;
+    // students += 1;
+    // students++;     // increment , used most in loops 
+
+    // // subtraction operator
+    // students = students -2 ;
+    // students -= 2;
+    // students--;     // decrement operator , subtracts 1 from variable 
+
+    // multiplication operator
+    // students*=2;
+
+    // //division operator
+    // students = students/3;
+    // // division fo rints will truncate the decimal
+    // students/=2;
+
+    // // modulus operator
+    // int remainder = students % 3;
+
+
+    std::cout << students;
+
+}
