1477d7934d345e5da9f1f340a97ddfd19e369227
jjosecodes <josejob123121@gmail.com>
2024-11-06 16:27:36 -0600

    struct barri code notes

--- ABDUL_BARRI_THE_GOAT/struct_abdul.cpp
+++ ABDUL_BARRI_THE_GOAT/struct_abdul.cpp
@@ -14,8 +14,15 @@ struct Rectangle{
 int main (){
 
     struct Rectangle r1= {10,5};
-
+    r1.length = 15;
+    r1.breath = 10;
+    r1.x = 'j';
     cout<< sizeof(r1)<<endl;
+
+    cout << "length is " << r1.length << endl;
+    cout<< "breath is " << r1.length << endl;
+    cout<<"char is " <<  r1.x << endl;
+
 
 
 return 0;
