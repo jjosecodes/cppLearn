7298c3a65fc314453423bdd32d531f3daf15cc0e
jjosecodes <josejob123121@gmail.com>
2024-11-07 16:58:14 -0600

    pointers abdul

--- ABDUL_BARRI_THE_GOAT/pointers_abdul.cpp
+++ ABDUL_BARRI_THE_GOAT/pointers_abdul.cpp
@@ -19,7 +19,24 @@ int main (){
     cout << *p << endl;
     // print the pointer itself , which is ADDRESS of a 
     cout << p << endl;
+    
+    int A[5] = {2,4,6,8,10};
+    //declare pointer to array
+    int *q;
+    // do not need & to reference array
+    q=A;
+    // or p=&A[0];
+    for ( int i = 0; i<5; i++ )
+    cout << q[i] << endl;
+    return 0;
+   // array in heap 
+   int *w = new int[5];
+    w[0] = 10; w[1] = 20; w[2] = 30; w[3] = 40; w[4] = 50;
+    for ( int i = 0; i<5; i++)
+    cout << w[i] <<endl;
+    delete [ ] w;
+
+
 
     return 0;
-
 }
